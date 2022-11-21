5fd54969b2580c4619e6899ece7a70a69ed1b8f7
Angelo Frangione <angelo@frangione.fr>
2022-11-21 19:49:10 +0100

    db: init with rusqlite; initial create db function

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,9 @@
 use super::DATABASE_FILENAME;
 
+use rusqlite::{Connection};
+
+static DATABASE_SQL: &str = include_str!("../database.sql");
+
 pub struct Database
 {
 	pub path: String
@@ -9,6 +13,12 @@ impl Database
 {
 	pub fn create(path: &String) -> Self
 	{
+		let connection = Connection::open(path.clone() + "/" + DATABASE_FILENAME).unwrap();
+		match connection.execute_batch(DATABASE_SQL)
+		{
+			Ok(_) => println!("success!"),
+			Err(e) => println!("error creating db: {:?}", e),
+		};
 		Database
 		{
 			path: path.clone() + DATABASE_FILENAME,
