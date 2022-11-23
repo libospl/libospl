87be85d7b2e7eb0644ceb86bbe41c20d6e2aa118
Alan Beveridge <adbeveridge@protonmail.com>
2022-11-23 12:36:40 -0800

    Run rustfmt

--- src/database.rs
+++ src/database.rs
@@ -1,35 +1,28 @@
-use super::DATABASE_FILENAME;
 use super::Error;
+use super::DATABASE_FILENAME;
 
-use rusqlite::{Connection};
+use rusqlite::Connection;
 
 static DATABASE_SQL: &str = include_str!("../database.sql");
 
-pub struct Database
-{
-	pub path: String
+pub struct Database {
+    pub path: String,
 }
 
-impl Database
-{
-	pub fn create(path: &String) -> Result<Self, Error>
-	{
-		let connection = match Connection::open(path.clone() + "/" + DATABASE_FILENAME)
-		{
-			Ok(c) => Ok(c),
-			Err(why) => return Err(Error::Other)
-		};
-		
-		match connection?.execute_batch(DATABASE_SQL)
-		{
-			Ok(_) =>
-			{
-				return Ok(Database
-				{
-					path: path.clone() + DATABASE_FILENAME,
-				})
-			},
-			Err(why) => return Err(Error::Other)
-		}
-	}
+impl Database {
+    pub fn create(path: &String) -> Result<Self, Error> {
+        let connection = match Connection::open(path.clone() + "/" + DATABASE_FILENAME) {
+            Ok(c) => Ok(c),
+            Err(why) => return Err(Error::Other),
+        };
+
+        match connection?.execute_batch(DATABASE_SQL) {
+            Ok(_) => {
+                return Ok(Database {
+                    path: path.clone() + DATABASE_FILENAME,
+                })
+            }
+            Err(why) => return Err(Error::Other),
+        }
+    }
 }
--- src/ospl.rs
+++ src/ospl.rs
@@ -12,153 +12,136 @@ use database::Database;
 use directory::Directory;
 
 #[derive(Debug)]
-pub enum Error
-{
-	Other = -1000,	// other error
-	Exists,			// the file or folder already exists
-	DB,				// database communication failed
-	NotFound,		// element not found
-	NotSupported,	// element not supported
-	Thumb,			// thumbnail creation failed
-	PhoNF,			// photo not found in db
-	AlbNF,			// album not found in db
+pub enum Error {
+    Other = -1000, // other error
+    Exists,        // the file or folder already exists
+    DB,            // database communication failed
+    NotFound,      // element not found
+    NotSupported,  // element not supported
+    Thumb,         // thumbnail creation failed
+    PhoNF,         // photo not found in db
+    AlbNF,         // album not found in db
 }
 
-pub struct Library
-{
-	path: String,
-	db: Database,
+pub struct Library {
+    path: String,
+    db: Database,
 }
 
-impl Library
-{
-	pub fn create(path: &String) -> Option<Self>
-	{
-		match Directory::from(&path)
-		{
-			Ok(res) =>
-			{
-				match Directory::create(res) {
-					Ok(_) => Some(Library
-						{
-							path: path.clone(),
-							db:
-							{
-								match Database::create(&path)
-								{
-									Ok(db) => db,
-									Err(_e) => return None
-								}
-							},
-						}),
-					Err(_e) => {
-						None
-					},
-				}
-				
-			},
-			Err(n) => {println!("{:?}", n); None},
-		}
-	}
+impl Library {
+    pub fn create(path: &String) -> Option<Self> {
+        match Directory::from(&path) {
+            Ok(res) => match Directory::create(res) {
+                Ok(_) => Some(Library {
+                    path: path.clone(),
+                    db: {
+                        match Database::create(&path) {
+                            Ok(db) => db,
+                            Err(_e) => return None,
+                        }
+                    },
+                }),
+                Err(_e) => {
+                    println!("{:?}", _e);
+                    None
+                }
+            },
+            Err(n) => {
+                println!("{:?}", n);
+                None
+            }
+        }
+    }
 }
 
 #[cfg(test)]
-mod tests
-{
-	use super::*;
+mod tests {
+    use super::*;
 
-	use rand::{thread_rng, Rng};
-	use rand::distributions::Alphanumeric;
+    use rand::distributions::Alphanumeric;
+    use rand::{thread_rng, Rng};
 
-	use rusqlite::{Connection};
+    use rusqlite::Connection;
 
-	static TEST_DIR: &str = "/tmp/";
-	static LIBRARY_CREATE_ERROR: &str = "error creating library";
+    static TEST_DIR: &str = "/tmp/";
+    static LIBRARY_CREATE_ERROR: &str = "error creating library";
 
-	fn remove_test_path(path: String)
-	{
-		println!("removing test dir");
-		match std::fs::remove_dir_all(path)
-		{
-			Ok(_) => {},
-			Err(e) => {println!("{:?}", e)}
-		}
-	}
+    fn remove_test_path(path: String) {
+        println!("removing test dir");
+        match std::fs::remove_dir_all(path) {
+            Ok(_) => {}
+            Err(e) => {
+                println!("{:?}", e)
+            }
+        }
+    }
 
-	fn generate_test_path() -> String
-	{
-		let rand_string: String = thread_rng()
-			.sample_iter(&Alphanumeric)
-			.take(30)
-			.map(char::from)
-			.collect();
-		TEST_DIR.to_string() + &rand_string + &LIBRARY_EXTENSION.to_string()
-	}
+    fn generate_test_path() -> String {
+        let rand_string: String = thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(30)
+            .map(char::from)
+            .collect();
+        TEST_DIR.to_string() + &rand_string + &LIBRARY_EXTENSION.to_string()
+    }
 
-	fn check_table_presence(name: &str, db: &str) -> bool
-	{
-		let conn = Connection::open(db).unwrap();
-		let mut conn = conn.prepare(
-			"SELECT name FROM sqlite_master WHERE type='table' AND name=?")
-			.unwrap();
-		let mut table: String = "".to_string();
-		let mut rows = conn.query([name]).unwrap();
-		while let Some(row) = rows.next().unwrap()
-		{
-			table = row.get(0).unwrap();
-		}
-		println!("check if table {} is present: table found: {}", name, table);
-		name.eq(&table)
-	}
+    fn check_table_presence(name: &str, db: &str) -> bool {
+        let conn = Connection::open(db).unwrap();
+        let mut conn = conn
+            .prepare("SELECT name FROM sqlite_master WHERE type='table' AND name=?")
+            .unwrap();
+        let mut table: String = "".to_string();
+        let mut rows = conn.query([name]).unwrap();
+        while let Some(row) = rows.next().unwrap() {
+            table = row.get(0).unwrap();
+        }
+        println!("check if table {} is present: table found: {}", name, table);
+        name.eq(&table)
+    }
 
-	#[test]
-	fn library_path()
-	{
-		let path = generate_test_path();
-		let _library = match Library::create(&path)
-		{
-			Some(lib) =>
-			{
-				println!("check if {} == {}", lib.path, path);
-				assert_eq!(lib.path, path);
-			},
-			None => {panic!("{}", LIBRARY_CREATE_ERROR)},
-		};
-		remove_test_path(path);
-	}
-	#[test]
-	fn library_database()
-	{
-		let path = generate_test_path();
-		let db_path = path.clone() + "/database.db";
+    #[test]
+    fn library_path() {
+        let path = generate_test_path();
+        let _library = match Library::create(&path) {
+            Some(lib) => {
+                println!("check if {} == {}", lib.path, path);
+                assert_eq!(lib.path, path);
+            }
+            None => {
+                panic!("{}", LIBRARY_CREATE_ERROR)
+            }
+        };
+        remove_test_path(path);
+    }
+    #[test]
+    fn library_database() {
+        let path = generate_test_path();
+        let db_path = path.clone() + "/database.db";
 
-		let _library = match Library::create(&path)
-		{
-			Some(_lib) =>
-			{
-				println!("checking if database has been created at {}", &db_path);
-				assert!(std::path::Path::new(&db_path).exists());
-			},
-			None => panic!("{}", LIBRARY_CREATE_ERROR),
-		};
-		assert!(check_table_presence("settings", &db_path));
-		assert!(check_table_presence("photos", &db_path));
-		assert!(check_table_presence("includes", &db_path));
-		assert!(check_table_presence("holds", &db_path));
-		assert!(check_table_presence("contains", &db_path));
-		assert!(check_table_presence("albums", &db_path));
-		remove_test_path(path);
-	}
+        let _library = match Library::create(&path) {
+            Some(_lib) => {
+                println!("checking if database has been created at {}", &db_path);
+                assert!(std::path::Path::new(&db_path).exists());
+            }
+            None => panic!("{}", LIBRARY_CREATE_ERROR),
+        };
+        assert!(check_table_presence("settings", &db_path));
+        assert!(check_table_presence("photos", &db_path));
+        assert!(check_table_presence("includes", &db_path));
+        assert!(check_table_presence("holds", &db_path));
+        assert!(check_table_presence("contains", &db_path));
+        assert!(check_table_presence("albums", &db_path));
+        remove_test_path(path);
+    }
 
-	#[test]
-	#[should_panic]
-	fn create_library_no_permissions()
-	{
-		let _library = match Library::create(&"/".to_string())
-		{
-			Some(_) => println!("trying to create library at path '/' should not return Some()"),
-			None => {panic!("could not create library at path '/'")}
-		};
-
-	}
+    #[test]
+    #[should_panic]
+    fn create_library_no_permissions() {
+        let _library = match Library::create(&"/".to_string()) {
+            Some(_) => println!("trying to create library at path '/' should not return Some()"),
+            None => {
+                panic!("could not create library at path '/'")
+            }
+        };
+    }
 }
