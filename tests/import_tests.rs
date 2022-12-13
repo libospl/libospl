35ebaffe2d06f4db983d251d78c02cdf94187cf8
Angelo Frangione <angelo@frangione.fr>
2022-12-13 14:22:32 +0100

    tests: use assert_eq to not panic
    This helps detect errors, when unexcepted panic is happening, instead of
    passing the test with any panic.

--- tests/import_tests.rs
+++ tests/import_tests.rs
@@ -51,43 +51,36 @@ mod tests
 	}
 
 	#[test]
-	#[should_panic]
 	fn import_single_photo_on_folder()
 	{
 		let path = generate_test_path();
 		let library = Library::create(&path).unwrap();
 
 		library.init().unwrap();
-		assert_ne!(library.import_photo("tests/files/test_folder/").err().unwrap(), Error::IsADirectory);
+		assert_eq!(library.import_photo("tests/files/test_folder/").err().unwrap(), Error::IsADirectory);
 		remove_test_path(path);
 	}
 
 	#[test]
-	#[should_panic]
 	fn import_photo_file_not_an_image()
 	{
 		let path = generate_test_path();
 		let library = Library::create(&path).unwrap();
 
 		library.init().unwrap();
-
-		assert_ne!(library.import_photo("tests/files/not_an_image.odt").err().unwrap(), Error::NotAnImage);
+		assert_eq!(library.import_photo("tests/files/not_an_image.odt").err().unwrap(), Error::NotAnImage);
 	}
 
 	#[test]
-	#[should_panic]
 	fn import_photo_file_not_valid()
 	{
 		let path = generate_test_path();
 		let library = Library::create(&path).unwrap();
-
 		library.init().unwrap();
-
-		assert_ne!(library.import_photo("tests/files/not_a_valid_file.png").err().unwrap(), Error::NotSupported);
+		assert_eq!(library.import_photo("tests/files/not_a_valid_file.png").err().unwrap(), Error::NotSupported);
 	}
 
 	#[test]
-	#[should_panic]
 	fn import_photo_no_string()
 	{
 		let path = generate_test_path();
@@ -95,7 +88,7 @@ mod tests
 
 		library.init().unwrap();
 
-		assert_ne!(library.import_photo("").err().unwrap(), Error::NotFound);
+		assert_eq!(library.import_photo("").err().unwrap(), Error::NotFound);
 	}
 }
 
