add37f5dc1ac6cda8f20088087e7f836d1fd268b
Angelo Frangione <angelo@frangione.fr>
2023-01-25 19:16:37 +0100

    tests: refactor tests to use Path

--- tests/collection_tests.rs
+++ tests/collection_tests.rs
@@ -1,53 +1,29 @@
+mod library_tests;
+use library_tests::generate_test_path;
+use library_tests::remove_test_path;
 
 #[cfg(test)]
 mod tests
 {
 	use ospl::Library;
-	use ospl::LIBRARY_EXTENSION;
 	use ospl::Error;
-
-	use rand::{thread_rng, Rng};
-	use rand::distributions::Alphanumeric;
-
-	static TEST_DIR: &str = env!("CARGO_TARGET_TMPDIR");
-
-	fn remove_test_path(path: String)
-	{
-		println!("removing test dir");
-		match std::fs::remove_dir_all(path)
-		{
-			Ok(_) => {},
-			Err(e) => {println!("{:?}", e)}
-		}
-	}
-
-	fn generate_test_path() -> String
-	{
-		let rand_string: String = thread_rng()
-			.sample_iter(&Alphanumeric)
-			.take(30)
-			.map(char::from)
-			.collect();
-		TEST_DIR.to_string() + "/" + &rand_string + &LIBRARY_EXTENSION.to_string()
-	}
-
 
 	#[test]
 	fn create_collection()
 	{
-		let path = generate_test_path();
+		let path = super::generate_test_path();
 		let library = Library::create(&path).unwrap();
 		match library.create_collection("2019", "Photos from 2019") {
 			Ok(_) => {},
 			Err(err) => {panic!("Error creating collection: {:?}", err)}
 		};
-		remove_test_path(path);
+		super::remove_test_path(path);
 	}
 
 	#[test]
 	fn get_id_not_exist()
 	{
-		let path = generate_test_path();
+		let path = super::generate_test_path();
 		let library = Library::create(&path).unwrap();
 
 		match library.get_collection_from_id(1)
@@ -61,13 +37,13 @@ mod tests
 			}
 			Ok(_) => panic!("error: should not return Ok() with an unexisting id")
 		}
-		remove_test_path(path);
+		super::remove_test_path(path);
 	}
 
 	#[test]
 	fn create_collection_test_values()
 	{
-		let path = generate_test_path();
+		let path = super::generate_test_path();
 		let library = Library::create(&path).unwrap();
 
 		let name = "2019";
@@ -77,6 +53,6 @@ mod tests
 		assert_eq!(name, collection.name());
 		assert_eq!(comment, collection.comment());
 
-		remove_test_path(path);
+		super::remove_test_path(path);
 	}
 }
