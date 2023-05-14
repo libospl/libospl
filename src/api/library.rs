d769b02e1dfc78babc32f473ecea92e5c54f544c
Angelo Frangione <angelo@frangione.fr>
2023-05-14 18:13:52 +0200

    clippy-fix: remove tabs in comments

--- src/api/library.rs
+++ src/api/library.rs
@@ -17,8 +17,8 @@ impl Library
 	/// # use ospl::Library;
 	/// let library = match Library::create("/my/awesome/path.ospl/")
 	/// {
-	/// 	Ok(_) => println!("Success!"),
-	/// 	Err(e) => println!("An error occured: {:?}", e),
+	///     Ok(_) => println!("Success!"),
+	///     Err(e) => println!("An error occured: {:?}", e),
 	/// };
 	///```
 	pub fn create<P: AsRef<Path>>(path: P) -> Result<Self, OsplError>
@@ -37,8 +37,8 @@ impl Library
 	/// # use ospl::Library;
 	/// let library = match Library::load("/my/awesome/path.ospl/")
 	/// {
-	/// 	Ok(_) => println!("Success!"),
-	/// 	Err(e) => println!("An error occured: {:?}", e),
+	///     Ok(_) => println!("Success!"),
+	///     Err(e) => println!("An error occured: {:?}", e),
 	/// };
 	/// ```
 	pub fn load<P: AsRef<Path>>(path: P) -> Result<Self, OsplError>
