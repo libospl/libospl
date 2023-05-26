93a2dd6ea0a046f2e1313a2c35c1d4629253a9a0
Angelo Frangione <angelo.frangione@gmail.com>
2023-05-26 22:28:45 +0000

    import: import a photo and a folder into an album

--- src/api/import.rs
+++ src/api/import.rs
@@ -31,4 +31,35 @@ impl Library
 		thumbnails::create_thumbnail_from_path(photo_path, self.fs.thumbnails_path().join(photo.get_filename()))?;
 		Ok(id)
 	}
+
+	pub fn import_photo_into_album<P: AsRef<Path>>(&self, photo_path: P, album_id: u32) -> Result<u32, OsplError>
+	{
+		if !photo_path.as_ref().exists()
+		{
+			return Err(OsplError::IoError(std::io::ErrorKind::NotFound));
+		}
+		let photo = self.import_photo(photo_path)?;
+		self.assign_photo_to_album(photo, album_id)?;
+		Ok(photo)
+	}
+
+	pub fn import_folder_into_album<P: AsRef<Path>>(&self, folder_path: P, album_id: u32) -> Result<Vec<Result<u32, OsplError>>, OsplError>
+	{
+		if !folder_path.as_ref().exists()
+		{
+			return Err(OsplError::IoError(std::io::ErrorKind::NotFound));
+		}
+		let mut results: Vec<Result<u32, OsplError>> = Vec::new();
+		println!("folder_path: {:?}", folder_path.as_ref());
+		for entry in std::fs::read_dir(folder_path)?
+		{
+			println!("entry: {:?}", entry);
+			let photo_path = entry?.path();
+			if photo_path.is_file()
+			{
+				results.push(self.import_photo_into_album(photo_path, album_id));
+			}
+		}
+		Ok(results)
+	}
 }
